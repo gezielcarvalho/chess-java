3e5f4c02943d79ac6b7cdd6263a984af09fddd90
Geziel Carvalho <geziel.natal@gmail.com>
2021-08-03 17:20:08 -0300

    Primeira classe Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,16 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Chess!");
+		
+		Position pos = new Position(1,2);
+		System.out.println(pos);
+		
 	}
 
 }
